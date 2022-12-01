66d393a810cacfc65226aa1bc09a192e0adf8e81
Temitope3665 <aroyewontope@gmail.com>
2022-12-01 16:54:35 +0100

    set up paraprhase key

--- solidwallet/src/templates/CreateWalletTemp/SetParaphraseKeyTemp.js
+++ solidwallet/src/templates/CreateWalletTemp/SetParaphraseKeyTemp.js
@@ -1,62 +1,119 @@
-import { Box, Divider, Flex, Text } from "@chakra-ui/react";
-import { securedIcon } from "../../assets/svgs/svg";
+import { Box, Divider, Flex, SimpleGrid, Text } from "@chakra-ui/react";
+import { copyIcon, generateIcon } from "../../assets/svgs/svg";
 import CustomButton from "../../components/CustomButton/customButton";
 import NavBar from "../../components/NavBar";
 
 const SetParaphraseKeyTemp = () => {
-    return (
-        <NavBar>
-        <Box w="100%">
-          <Box
-            w="40%"
-            mx="auto"
-            my="30px"
-            p="20px"
-            boxShadow="rgba(0, 0, 0, 0.05) 0px 0px 0px 1px"
-            borderRadius="8px"
-          >
-            <Text fontSize="34px" fontWeight="bold">
-              Choose a Security Method
+  const words = [
+    "rythm",
+    "fun",
+    "rick",
+    "gap",
+    "yellow",
+    "mistake",
+    "joke",
+    "zoo",
+    "manual",
+    "wood",
+  ];
+  return (
+    <NavBar>
+      <Box w="100%">
+        <Box
+          w="40%"
+          mx="auto"
+          my="30px"
+          p="20px"
+          boxShadow="rgba(0, 0, 0, 0.05) 0px 0px 0px 1px"
+          borderRadius="8px"
+        >
+          <Text fontSize="40px" fontWeight="bold">
+            Setup Your Secure PassPhrase
+          </Text>
+          <Divider />
+          <Box my="10px" color="brand.gray">
+            <Text>
+              Ensure to write down the following key words in order and keep
+              them safe somewhere.
             </Text>
-            <Divider />
-            <Box my="10px" color="brand.gray">
-              <Text>
-                Select a method to secure and recover your account. This will be
-                used to verify important activity, recover your account and access
-                your account from other devices.
-              </Text>
-  
-              <Flex bg="#F0F9FE" p="10px" mt="20px" borderRadius="" borderLeftWidth="8px" borderLeftColor="black" alignItems="center" justifyContent="space-between">
-                  <Box>
-                      <Text color="black" fontWeight="bold" fontSize="18px">Secure Passphrase</Text>
-                      <Text>Generate and safely store a unique passphrase.</Text>
+
+            <Box
+              boxShadow="rgba(17, 17, 26, 0.05) 0px 4px 16px, rgba(17, 17, 26, 0.05) 0px 8px 32px"
+              p="15px"
+              mt="20px"
+              borderRadius="4px"
+              mx="10px"
+            >
+              <SimpleGrid columns={3} gap="15px">
+                {words.map((res, index) => (
+                  <Box bg="#F9F9F9" color="black">
+                    <Flex
+                      alignItems="center"
+                      fontSize="14px"
+                      py="10px"
+                      mx="20px"
+                    >
+                      <Text color="brand.gray">{index + 1}</Text>
+                      <Text ml="10px">{res}</Text>
+                    </Flex>
                   </Box>
-                  <Box>{securedIcon}</Box>
-              </Flex>
-              <CustomButton
-                w="100%"
-                bg="black"
-                border="1px solid black"
-                color="white"
-                hoverBg="white"
-                hoverColor="black"
-                testid="on-close"
-                href="/set-recovery-method"
-                mt="40px"
-              >
-                Secure My Account
-              </CustomButton>
+                ))}
+              </SimpleGrid>
             </Box>
-          </Box>
-          <Box color="brand.gray" textAlign="center">
-            <Text>Already have a wallet ?</Text>
-            <Text as="u" mt="10px" cursor="pointer">
-              Import Existing Wallet
-            </Text>
+
+            <Flex justifyContent="space-evenly" w="100%" mx="auto" mt="20px">
+              <CustomButton
+                w="180px"
+                bg="brand.white"
+                border="1px solid black"
+                color="black"
+                hoverBg="black"
+                hoverColor="brand.white"
+                testid="on-close"
+                // href="/create-wallet"
+              >
+                <Box>{copyIcon}</Box>
+                <Text ml="15px">Copy</Text>
+              </CustomButton>
+
+              <CustomButton
+                w="180px"
+                bg="brand.white"
+                border="1px solid black"
+                color="black"
+                hoverBg="black"
+                hoverColor="brand.white"
+                testid="on-close"
+              >
+                <Box>{generateIcon}</Box>
+                <Text ml="15px">Generate New</Text>
+              </CustomButton>
+            </Flex>
+
+            <CustomButton
+              w="100%"
+              bg="black"
+              border="1px solid black"
+              color="white"
+              hoverBg="white"
+              hoverColor="black"
+              testid="on-close"
+              href="/set-recovery-method"
+              mt="40px"
+            >
+              Proceed
+            </CustomButton>
           </Box>
         </Box>
-      </NavBar>
-    )
+        <Box color="brand.gray" textAlign="center">
+          <Text>Already have a wallet ?</Text>
+          <Text as="u" mt="10px" cursor="pointer">
+            Import Existing Wallet
+          </Text>
+        </Box>
+      </Box>
+    </NavBar>
+  );
 };
 
 export default SetParaphraseKeyTemp;
