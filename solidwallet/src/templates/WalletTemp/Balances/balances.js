78b3f21bac3486c5c92c28e1e18de0362a31ea89
Temitope3665 <aroyewontope@gmail.com>
2022-12-01 20:16:43 +0100

    wallet UI updated

--- solidwallet/src/templates/WalletTemp/Balances/balances.js
+++ solidwallet/src/templates/WalletTemp/Balances/balances.js
@@ -1,17 +1,40 @@
-import { Box, SimpleGrid, Text } from "@chakra-ui/react";
-import { sendIcon } from "../../../assets/svgs/svg";
+import { Box, Divider, SimpleGrid, Text } from "@chakra-ui/react";
+import { plusIcon, receiveIcon, sendIcon, swapIcon } from "../../../assets/svgs/svg";
 
 const Balances = () => {
     return (
-        <Box>
+        <Box w="100%" textAlign="center">
             <Text fontSize="40px" fontWeight="bold">0 USD</Text>
             <Text color="brand.gray">Available balance</Text>
 
-            <SimpleGrid colums={4} gap="20px">
-                <Box bg="black" borderRadius="20px" p="25px">
-                    {sendIcon}
+            <SimpleGrid columns={4} gap="20px" mt="60px" justifyContent="center" placeItems="center" mx="150px" fontSize="14px">
+                <Box cursor="pointer">
+                    <Box bg="black" borderRadius="15px" p="15px" w="55px" textAlign="center">
+                        {sendIcon}
+                    </Box>
+                    <Text mt="5px">Send</Text>
+                </Box>
+                <Box cursor="pointer">
+                    <Box bg="black" borderRadius="15px" p="15px" w="55px" textAlign="center">
+                        {receiveIcon}
+                    </Box>
+                    <Text mt="5px">Receive</Text>
+                </Box>
+                <Box cursor="pointer">
+                    <Box bg="black" borderRadius="15px" p="15px" w="55px" textAlign="center">
+                        {plusIcon}
+                    </Box>
+                    <Text mt="5px">Top Up</Text>
+                </Box>
+                <Box cursor="pointer">
+                    <Box bg="black" borderRadius="15px" p="15px" w="55px" textAlign="center">
+                        {swapIcon}
+                    </Box>
+                    <Text mt="5px">Swap</Text>
                 </Box>
             </SimpleGrid>
+
+            <Divider my="30px" />
         </Box>
     )
 };
--- solidwallet/src/templates/WalletTemp/index.js
+++ solidwallet/src/templates/WalletTemp/index.js
@@ -7,14 +7,14 @@ const WalletTemp = () => {
     const [showBalance, setShowBalance] = useState(true);
     return (
         <AuthNavBar>
-            <Box mx="120px" mt="80px">
+            <Box mx="120px" mt="100px">
                 <Flex w="100%">
-                    <Box boxShadow="rgba(0, 0, 0, 0.09) 0px 3px 12px" w="80%" borderRadius="4px">
+                    <Box boxShadow="rgba(0, 0, 0, 0.09) 0px 3px 12px" w="60%" borderRadius="4px" border="1px solid #9C9C9C">
                         <Flex fontWeight="bold" fontSize="25px" alignItems="center" justifyContent="space-between">
                             <Text color={!showBalance && "brand.gray"} onClick={() => setShowBalance(true)} cursor="pointer" bg={showBalance && "#FAFAFA"} py="20px" w="50%" textAlign="center">Balances</Text>
                             <Text color={showBalance && "brand.gray"} onClick={() => setShowBalance(false)} cursor="pointer" bg={!showBalance && "#FAFAFA"} py="20px" w="50%" textAlign="center">Collectibles</Text>
                         </Flex>
-                        <Box>
+                        <Box my="60px">
                             {showBalance ? <Balances /> : null}
                         </Box>
                     </Box>
