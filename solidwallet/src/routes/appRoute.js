0cc67940dce5e36bf473aa61b51da1cde4483287
Hexdee <lukmansaheed79@gmail.com>
2022-12-02 21:20:56 +0100

    Restructure create-wallet page

--- solidwallet/src/routes/appRoute.js
+++ solidwallet/src/routes/appRoute.js
@@ -5,9 +5,6 @@ import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../theme";
 import Home from "../pages";
 import { CreateWallet } from "../pages/createWallet";
-import SetRecoveryMethod from "../pages/setRecoveryMethod";
-import SetParaphraseKey from "../pages/SetParaphraseKey";
-import VerifyKey from "../pages/VerifyKey";
 import WalletHome from "../pages/walletHome";
 import SendMoney from "../pages/SendMoney";
 import ReceiveMoney from "../pages/ReceiveMoney";
@@ -16,18 +13,15 @@ const AppRoute = () => {
 
   return render(
     <BrowserRouter>
-     <ChakraProvider theme={theme} resetCSS>
-      <Routes>
-        <Route index path="/" element={<Home />} />
-        <Route path="/create-wallet" element={<CreateWallet />} />
-        <Route path="/set-recovery-method" element={<SetRecoveryMethod />} />
-        <Route path="/setup-paraphrase-key" element={<SetParaphraseKey />} />
-        <Route path="/verify-paraphrase-key" element={<VerifyKey />} />
-        <Route path="/wallet" element={<WalletHome />} />
-        <Route path="/send-money" element={<SendMoney />} />
-        <Route path="/receive-money" element={<ReceiveMoney />} />
-      </Routes>
-     </ChakraProvider>
+      <ChakraProvider theme={theme} resetCSS>
+        <Routes>
+          <Route index path="/" element={<Home />} />
+          <Route path="/create-wallet" element={<CreateWallet />} />
+          <Route path="/wallet" element={<WalletHome />} />
+          <Route path="/send-money" element={<SendMoney />} />
+          <Route path="/receive-money" element={<ReceiveMoney />} />
+        </Routes>
+      </ChakraProvider>
     </BrowserRouter>,
     document.getElementById("root")
   );
